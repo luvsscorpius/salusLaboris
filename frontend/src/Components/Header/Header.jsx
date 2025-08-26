bd60e1129f49378f86db8a6a3c6fddf36e69cb75
and <andersonvittor1801@gmail.com>
2025-08-26 10:11:39 -0300

    style(header.jsx): styling header

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as H from './Styles'
 import { IoMenuSharp } from "react-icons/io5";
 import logo from '../../assets/logo.webp'
 
 export default function Header() {
+  const [open, setOpen] = useState(false)
+
   return (
     <H.header>
         <img src={logo} alt="Logo da header" />
-        <IoMenuSharp />
+
+        <H.navLinks open={open}>
+          <ul>
+            <li href="">Home</li>
+            <li href="">Serviços</li>
+            <li href="">Sobre</li>
+            <li href="">Blog</li>
+            <li href="">Contato</li>
+            <li href="">Login</li>
+          </ul>
+        </H.navLinks>
+
+        <IoMenuSharp onClick={() => setOpen(!open)} />
     </H.header>
   )
 }
--- frontend/src/Components/Header/Styles.jsx
+++ frontend/src/Components/Header/Styles.jsx
@@ -15,9 +15,50 @@ export const header = styled.header`
     svg {
         font-size: 40px;
         color: #4A7C59;
+        transition: transform 0.8s ease;
 
         &:hover {
             cursor: pointer;
+            opacity: 0.8;
+            transform: scale(0.9);
+        }
+    }
+`
+
+export const navLinks = styled.nav`
+    position: absolute;
+    top: 60px;
+    right: 20px;
+    width: 35%;
+    border-radius: 10px;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+    overflow: hidden;
+  
+    max-height: ${({ open }) => (open ? "500px" : "0")};
+    opacity: ${({ open }) => (open ? "1" : "0.7")};
+    transition: all 0.5s ease;
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        font-family: "Poppins", sans-serif;
+        font-weight: 800;
+        font-size: 15px;
+        padding: 10px 0 10px 0;
+        
+        li {
+            color: #4A7C59;
+            list-style: none;
+            transition: transform 0.8s ease;
+            text-align: center;
+
+            &:hover {
+                cursor: pointer;
+                opacity: 0.8;
+                transform: scale(0.93);
+            }
         }
     }
 `
