faa1493e337315567900af01e09117ddcaa91de6
and <andersonvittor1801@gmail.com>
2025-09-02 18:11:55 -0300

    feat(parceiros.jsx): continuing styling parceiros.jsx

--- frontend/src/Components/Parceiros/Styles.jsx
+++ frontend/src/Components/Parceiros/Styles.jsx
@@ -1,1 +1,112 @@
 import styled from 'styled-components'
+
+export const main = styled.main`
+    font-family: "Poppins", sans-serif; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 60px;
+
+    .firstTitle {
+        padding: 50px 0px 0px 0px;
+    }
+
+    .secondTitle {
+        gap: 40px;
+    }
+`
+
+export const titleContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+        font-size: 30px;
+        color: #4A7C59;
+        font-weight: 800;
+        text-align: center;
+        padding: 0px 10px 0px 10px;
+    }
+
+    h3 {
+        font-size: 22px;
+        color: #1d1d1d;
+        font-weight: 700;
+        text-align: center;
+        opacity: 0.90;
+        padding: 0px 10px 0px 10px;
+    }
+
+    h4 {
+        font-size: 25px;
+        text-align: center;
+        color: #4A7C59;
+        padding: 0 40px 0 40px;
+    }
+
+    button {
+        border: none;
+        height: 60px;
+        width: 70%;
+        background-color: #4A7C59;
+        color: #fff;
+        font-weight: 600;
+        font-size: 18px;
+        border-radius: 5px;
+        transition: transform 0.3s ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            transform: scale(0.98);
+        }
+    }
+`
+
+export const textContainer = styled.div`
+    p {
+        padding: 0px 40px 0px 40px;
+        text-align: justify;
+        font-size: 24px;
+        font-weight: 100;
+        color: #1d1d1d;
+    }
+
+    span {
+        background-color: #eee;
+        color: #4A7C59;
+        font-weight: 800;
+    }
+`
+
+export const empresasContainer = styled.div`
+    width: 100%;
+    padding: 5px;
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+    gap: 25px;
+    overflow-x: auto; 
+    overflow-y: hidden; 
+    scroll-behavior: smooth;
+
+    &::-webkit-scrollbar {
+        display: none;
+    }
+
+    img {
+        width: 150px;
+        scroll-snap-align: start;
+    }
+
+    @media (min-width: 1024px) {
+        justify-content: center;
+
+        &::-webkit-scrollbar {
+            display: grid;
+        }
+    }
+`
