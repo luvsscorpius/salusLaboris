4faefc0663b8f4560cceabe4067e41a99e534610
and <andersonvittor1801@gmail.com>
2025-09-05 09:35:02 -0300

    feat(missao.jsx): starting to develop missao component

--- frontend/src/Components/Missao/Missao.jsx
+++ frontend/src/Components/Missao/Missao.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import * as M from './Styles'
+import missaoImage from '../../assets/missaoImage.webp'
 
 export const Missao = () => {
   return (
-    <M.main>Missao</M.main>
+    <M.main >
+        <img src={missaoImage} alt="Imagem de background do componente missão, visão e valores" />
+    </M.main  >
   )
 }
--- frontend/src/Components/Missao/Styles.jsx
+++ frontend/src/Components/Missao/Styles.jsx
@@ -2,4 +2,13 @@ import styled from 'styled-components'
 
 export const main = styled.main`
     border: 1px solid red;
+    background-color: #233D4D;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: 0.3;
+        z-index: 0;
+    }
 `
