a334f2a0f6fb839d745f5fedf5f5869179d58cc1
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-09-05 12:22:23 -0300

    feat(missao.jsx): implementing infoContainer & content

--- frontend/src/Components/Missao/Missao.jsx
+++ frontend/src/Components/Missao/Missao.jsx
@@ -6,6 +6,33 @@ export const Missao = () => {
   return (
     <M.main >
         <img src={missaoImage} alt="Imagem de background do componente missão, visão e valores" />
+
+        <M.titleContainer>
+          <h1>COMO FAZEMOS?</h1>
+        </M.titleContainer>
+
+        <M.infoContainer>
+          <M.infoContent>
+
+            <h2>MISSÃO</h2>
+            <p>Promover saúde e segurança no trabalho com soluções integradas e humanizadas, através de uma equipe multidisciplinar qualificada, garantindo o bem-estar dos colaboradores e a sustentabilidade das empresas em todo o Brasil.</p>
+
+          </M.infoContent>
+
+          <M.infoContent>
+
+            <h2>VISÃO</h2>
+            <p>Ser referência nacional em saúde ocupacional, reconhecida pelo compromisso com ambientes de trabalho mais seguros, saudáveis e socialmente responsáveis.</p>
+
+          </M.infoContent>
+
+          <M.infoContent>
+
+            <h2>VALORES</h2>
+            <p>Ética, responsabilidade social, excelência técnica, humanização, acessibilidade e colaboração.</p>
+
+          </M.infoContent>
+        </M.infoContainer>
     </M.main  >
   )
 }
--- frontend/src/Components/Missao/Styles.jsx
+++ frontend/src/Components/Missao/Styles.jsx
@@ -1,14 +1,70 @@
 import styled from 'styled-components'
 
 export const main = styled.main`
-    border: 1px solid red;
     background-color: #233D4D;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    font-family: "Poppins", sans-serif;
+    padding: 100px 0 100px 0;
+    gap: 30px;
 
     img {
+        position: absolute;
         width: 100%;
         height: 100%;
         object-fit: cover;
         opacity: 0.3;
         z-index: 0;
+    }
+`
+
+export const titleContainer = styled.div`
+    align-items: center;
+    justify-content: center;
+    display: flex;
+    width: 100%;
+
+    h1 {
+        color: #4A7C59;
+        background-color: #eee;
+        z-index: 10;
+        padding: 2px;
+        font-size: 34px;
+    }
+`
+
+export const infoContainer = styled.div` 
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+` 
+
+export const infoContent = styled.div` 
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px;
+
+    h2 {
+        font-size: 28px;
+        color: #4A7C59;
+        background-color: #eee;
+        text-align: start;
+        z-index: 10;
+        font-weight: 800;
+        width: fit-content; 
+        padding: 2px;
+    }
+
+    p {
+        font-size: 20px;
+        text-align: justify;
+        color: #fff;
+        z-index: 10;
+        font-weight: 100;
     }
 `
