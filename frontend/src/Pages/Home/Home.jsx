b8d925bf2511d06a0f53d14d3424ee5dff6c0435
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-09-09 18:16:21 -0300

    feat(blog.jsx): starting to develop blog component

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import { Servicos } from '../../Components/Servicos/Servicos'
 import { Parceiros } from '../../Components/Parceiros/Parceiros'
 import { Missao } from '../../Components/Missao/Missao'
 import { Feedbacks } from '../../Components/Feedbacks/Feedbacks'
+import { Blog } from '../../Components/Blog/Blog'
 
 export const Home = () => {
   return (
@@ -16,6 +17,7 @@ export const Home = () => {
         <Parceiros/>
         <Missao/>
         <Feedbacks/>
+        <Blog/>
     </H.home>
   )
 }
