6bd62d61706529df7369ab63997e132465b1fd70
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-09-09 18:32:30 -0300

    feat(blog.jsx): starting to develop blog component

--- frontend/src/Components/Blog/Blog.jsx
+++ frontend/src/Components/Blog/Blog.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import * as B from './Styles'
 
 export const Blog = () => {
   return (
-    <div>Blog</div>
+    <B.main>
+        
+        <B.titleContainer>
+            <h2>BLOG DA SALUS LABORIS</h2>
+            <h3>EXPANDA SEU CONHECIMENTO SOBRE SEGURANÇA DO TRABALHO</h3>
+        </B.titleContainer>
+
+        <B.cardsContainer>
+            <B.card>
+                <B.cardHeader>
+                    <span>
+                        <p>Segurança do Trabalho</p>
+                    </span>
+                </B.cardHeader>
+            </B.card>
+        </B.cardsContainer>
+    </B.main>
   )
 }
--- frontend/src/Components/Blog/Styles.jsx
+++ frontend/src/Components/Blog/Styles.jsx
@@ -1,1 +1,49 @@
 import styled from 'styled-components'
+
+export const main = styled.main` 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #233D4D;
+    font-family: "Poppins", sans-serif;
+    padding: 50px 0 50px 0;
+    gap: 50px;
+`
+
+export const titleContainer = styled.div`
+    width: 90%;
+
+    h2 {
+        text-align: center;
+        color: #4A7C59;
+        font-size: 34px;
+        font-weight: 800;
+    }
+
+    h3 {
+        text-align: center;
+        color: #FFFFFF;
+        font-size: 20px;
+        font-weight: 800;
+        opacity: 0.90;
+    }
+`
+
+export const cardsContainer = styled.div` 
+    width: 90%;
+    padding: 0 10px 0 10px;
+`
+
+export const card = styled.div`
+    border: 1px solid red;
+`
+
+export const cardHeader = styled.div`
+    border: 1px solid red;
+    height: 100px;
+    
+    p {
+        padding: 10px 0px 0 10px;
+    }
+`
