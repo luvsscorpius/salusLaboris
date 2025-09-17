44f9eb441a399f8f1e72d7324bf543effad196e4
and <andersonvittor1801@gmail.com>
2025-09-17 16:11:32 -0300

    fix(localizacao.jsx): fixing marker

--- frontend/src/Components/Localizacao/Localizacao.jsx
+++ frontend/src/Components/Localizacao/Localizacao.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import * as L from './Styles'
-import { Marker, Popup } from "react-leaflet"
+import { Marker, Popup, MapContainer } from "react-leaflet"
 import { TileLayer} from 'react-leaflet/TileLayer'
-import { MapContainer, useMap } from 'react-leaflet'
+import {Icon} from 'leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
 
 export const Localizacao = () => {
   return (
@@ -14,18 +15,24 @@ export const Localizacao = () => {
         </L.titleContainer>
 
         <L.mapContainer>
-          <MapContainer center={[-22.257069296647767, -45.693783837411885]} zoom={50} scrollWheelZoom={false}>
+          <MapContainer center={[-22.257069296647767, -45.693783837411885]} zoom={35} scrollWheelZoom={true}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-22.257069296647767, -45.693783837411885]} >
+            <Marker position={[-22.257069296647767, -45.693783837411885]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
               <Popup>
                 Nossa Localização <br /> 
               </Popup>
             </Marker>
           </MapContainer>
         </L.mapContainer>
+
+
+        <L.descContainer>
+           <h3>Santa Rita do Sapucaí - MG, 37537-230</h3>
+           <p>Av. Francisco Bilac Pinto, 95 - sala 1 - Monte Belo</p>
+        </L.descContainer>
       </L.locationContainer>
     </L.main>
   )
--- frontend/src/Components/Localizacao/Styles.jsx
+++ frontend/src/Components/Localizacao/Styles.jsx
@@ -15,6 +15,7 @@ export const locationContainer = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    gap: 50px;
 `
 
 export const titleContainer = styled.div`
@@ -31,7 +32,7 @@ export const titleContainer = styled.div`
 
     h2 {
         text-align: center;
-        font-size: 34px;   
+        font-size: 30px;   
         font-weight: 800; 
         color: #4A7C59;
     }
@@ -39,11 +40,36 @@ export const titleContainer = styled.div`
 
 export const mapContainer = styled.div`
     width: 90%;
-    padding: 50px 0 0 0;
 
     .leaflet-container {
         width: 100%;
         height: 400px;
+    }
+`
+
+export const descContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    padding: 0 10px 0 10px;
+    width: 90%;
+
+    h3 {
+        color: #1d1d1d;
+        opacity: 0.85;
+        font-weight: 800;
+        text-align: center;
+        font-size: 18px;
+    }
+
+    p {
+        color: #4A7C59;
+        opacity: 0.85;
+        font-weight: 800;
+        text-align: center;
+        font-size: 14px;
     }
 `
 
