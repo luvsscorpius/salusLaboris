20f5bea0fc28b2a0117066c1e858ac47bc5d4bc1
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-09-18 18:36:58 -0300

    feat(footer.jsx): implementing footer.jsx

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -3,6 +3,17 @@ import * as F from './Styles'
 
 export const Footer = () => {
   return (
-    <F.footer>Footer</F.footer>
+    <F.footer>
+        <F.titleContainer>
+            <h3>CUIDAR DE QUEM TRABALHA É CONSTRUIR UM FUTURO MAIS SAUDÁVEL PARA TODOS</h3>
+            <h2>SEGURANÇA NÃO É CUSTO. <br /> É VALOR.</h2>
+        </F.titleContainer>
+
+        <F.atendimentoContainer>
+            <h4>ATENDIMENTO</h4>
+
+            <p>SEGUNDA – SEXTA: 09h30 – 17h30 <br />SÁBADO – DOMINGO: FECHADO</p>
+        </F.atendimentoContainer>
+    </F.footer>
   )
 }
--- frontend/src/Components/Footer/Styles.jsx
+++ frontend/src/Components/Footer/Styles.jsx
@@ -6,8 +6,54 @@ export const footer = styled.footer`
     align-items: center;
     justify-content: center;
     padding: 50px 0 50px 0;
+    font-family: "Poppins", sans-serif;
+    background-color: #233D4D;
+    gap: 50px;
 `
 
 export const titleContainer = styled.div` 
-    border: 1px solid red;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+
+    h3 {
+        text-align: center;
+        font-weight: 800;
+        color: #fff;
+        opacity: 0.85;
+        font-size: 18px;
+    }
+
+    h2 {
+        text-align: center;
+        font-weight: 800;
+        color: #4A7C59;
+        opacity: 0.85;
+        font-size: 30px;
+    }
+`
+
+export const atendimentoContainer = styled.div`
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    color: #fff;
+    opacity: 0.85;
+
+    h4 {
+        font-weight: 800;
+        font-size: 18px;
+    }
+
+    p {
+        text-align: center;
+        font-weight: 400;
+        font-size: 16px;
+    }
 `
