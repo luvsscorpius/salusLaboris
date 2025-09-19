dd15ad03c67e5db9d161fbf73704ca639cf73fb9
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-09-19 14:12:48 -0300

    feat(footer.jsx): continuing developing footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
 import * as F from './Styles'
+import Blob from '../../assets/blob-scene-haikei.svg'
 
 export const Footer = () => {
   return (
     <F.footer>
-        <F.titleContainer>
-            <h3>CUIDAR DE QUEM TRABALHA É CONSTRUIR UM FUTURO MAIS SAUDÁVEL PARA TODOS</h3>
-            <h2>SEGURANÇA NÃO É CUSTO. <br /> É VALOR.</h2>
-        </F.titleContainer>
+      <F.titleContainer>
+        <h3>CUIDAR DE QUEM TRABALHA É CONSTRUIR UM FUTURO MAIS SAUDÁVEL PARA TODOS</h3>
+        <h2>SEGURANÇA NÃO É CUSTO. <br /> É VALOR.</h2>
+      </F.titleContainer>
 
-        <F.atendimentoContainer>
-            <h4>ATENDIMENTO</h4>
+      <F.atendimentoContainer>
+        <h4>ATENDIMENTO</h4>
 
-            <p>SEGUNDA – SEXTA: 09h30 – 17h30 <br />SÁBADO – DOMINGO: FECHADO</p>
-        </F.atendimentoContainer>
+        <p>SEGUNDA – SEXTA: 09h30 – 17h30 <br />SÁBADO – DOMINGO: FECHADO</p>
+      </F.atendimentoContainer>
+
+      <F.mailingContainer>
+        <F.mailingCard>
+
+          <F.mainlingHeader>
+            <h3>MAILING LIST</h3>
+          </F.mainlingHeader>
+
+          <F.mainlingBody>
+            <img src={Blob} alt="Fundo do card" />
+
+            <p>Cadastre seu e-mail e faça parte da nossa comunidade comprometida com ambientes de trabalho mais seguros e saudáveis.</p>
+
+            <span className="inputContainer">
+              <label>Email:</label>
+              <input type="email" name="email" />
+            </span>
+
+            <button>QUERO RECEBER</button>
+          </F.mainlingBody>
+
+        </F.mailingCard>
+      </F.mailingContainer>
     </F.footer>
   )
 }
--- frontend/src/Components/Footer/Styles.jsx
+++ frontend/src/Components/Footer/Styles.jsx
@@ -57,3 +57,88 @@ export const atendimentoContainer = styled.div`
         font-size: 16px;
     }
 `
+
+export const mailingContainer = styled.div` 
+    width: 90%;
+`
+
+export const mailingCard = styled.div`
+`
+
+export const mainlingHeader = styled.div` 
+    background-color: #4A7C59;
+    padding: 10px 0;
+    border-radius: 10px 10px 0 0;
+
+    h3 {
+        text-align: center;
+        color: #fff;
+        opacity: 0.85;
+    }
+`
+
+export const mainlingBody = styled.div`
+    padding: 10px 10px;
+    color: #fff;
+    opacity: 0.85;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    position: relative;
+    z-index: 10;
+    background-color: #233D4D;
+
+    img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        right: 0;
+        object-fit: cover;
+        z-index: 2;
+        opacity: 0.50;
+    }
+
+    .inputContainer {
+        display: flex;
+        flex-direction: column;
+        z-index: 10;
+    }
+
+    p {
+        text-align: justify;
+        font-size: 18px;
+        z-index: 10;
+    }
+
+    label {
+        font-size: 18px;
+        z-index: 10;
+    }
+
+    input {
+        height: 48px;
+        border-radius: 5px;
+        z-index: 10;
+        border: none;
+        padding: 0 0px 0 10px;
+        font-size: 18px;
+    }
+
+    button {
+        font-size: 20px;
+        height: 48px;
+        background-color: #4A7C59;
+        color: #fff;
+        border: none;
+        font-weight: 800;
+        border-radius: 5px;
+        z-index: 10;
+
+        &:hover {
+            cursor: pointer;
+            opacity: 0.96;
+            transition: 0.3s ease-in-out;
+        }
+    }
+`
