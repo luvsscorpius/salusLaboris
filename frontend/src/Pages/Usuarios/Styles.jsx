acfb1d695507fa4c73cd409b12ae178c8cad15f5
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-12-04 19:49:02 -0300

    feat(usuarios.jsx): implementing page to show users

--- frontend/src/Pages/Usuarios/Styles.jsx
+++ frontend/src/Pages/Usuarios/Styles.jsx
@@ -2,4 +2,20 @@ import styled from "styled-components";
 
 export const main = styled.main`
     font-family: "Poppins", sans-serif;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    height: auto;
+
+    .header {
+        width: 90%;
+        padding: 20px 20px;
+    }
+
+    @media (min-width: 1024px) {
+        flex-direction: row;
+        align-items: start;
+        height: 100%;
+    }
 `
--- frontend/src/Pages/Usuarios/Usuarios.jsx
+++ frontend/src/Pages/Usuarios/Usuarios.jsx
@@ -1,12 +1,84 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import { Menu } from '../../Components/Menu/Menu'
-import * as M from '../../Components/Menu/Styles'
-import * as U from './Styles'
+import * as B from '../Blog/Styles'
+import { CiCirclePlus } from "react-icons/ci";
+import * as G from '../GerenciarPosts/Styles'
+import { SalusContext } from '../../Context/Context';
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import ReactPaginate from 'react-paginate'
 
 export const Usuarios = () => {
-  return (  
-    <U.main>
-      <Menu/>
-    </U.main>
+
+  const {users} = useContext(SalusContext)
+
+  const [currentPage, setCurrentPage] = useState(0)
+  const itemsPage = 5
+
+  const handlePageClick = ({ selected }) => {
+      setCurrentPage(selected)
+  }
+
+  const offSet = currentPage * itemsPage
+  const currentPageData = users.slice(offSet, offSet + itemsPage)
+
+  return (
+    <G.main>
+      <Menu />
+
+      <G.gerenciarPostsBody>
+        <B.blogHeader className='header'>
+          <div>
+            <h2>Gerenciar Usuários</h2>
+            <button onClick={() => window.open("/adm/criarusuario", "_self")}><CiCirclePlus size={45} /></button>
+          </div>
+
+          <hr />
+        </B.blogHeader>
+
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Descrição</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users && users.length > 0 && currentPageData.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name.length > 200
+                  ? user.name.substring(0, 200) + "..."
+                  : user.name}</td>
+                <td className='data'>{user.email.length > 45 ? user.email.substring(0, 45) + "..." : user.email}</td>
+                <td className='autor'>{user.desc.length > 50 ? user.desc.substring(0, 50) + "..." : user.desc}</td>
+                <td className='icon'>
+                  <FaRegEdit size={25}  />
+                  <MdDeleteOutline size={25} />
+                </td>
+              </tr>
+            ))}
+
+          </tbody>
+        </table>
+
+        <ReactPaginate
+          pageCount={Math.ceil(users.length / itemsPage)}
+          pageRangeDisplayed={3} // Número de páginas a serem exibidas
+          marginPagesDisplayed={1} // Número de páginas a serem exibidas nas extremidades
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+          nextLinkClassName={"next"}
+          nextLabel=">"
+          previousLabel="<"
+          previousLinkClassName={"previous"}
+          pageClassName={"page"} />
+
+      </G.gerenciarPostsBody>
+
+    </G.main>
   )
 }
