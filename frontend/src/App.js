a7f719ce4fae171a7dfb82777d80fef10924fa94
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2025-12-06 21:47:43 -0300

    fix(fix): fixing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
     <>
       <GlobalStyle />
       <ToastContainer autoClose={3000} className="toast-container" />
-      <Router basename="https://github.com/luvsscorpius/salusLaboris/">
+      <Router basename="/salusLaboris/">
         <SalusProvider>
           <Header />
           <Routes>
