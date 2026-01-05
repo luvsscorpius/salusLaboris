47e374b95e4fbb59a835d91987201ec31daf5c48
and <andersonvittor1801@gmail.com>
2026-01-05 14:56:10 -0300

    feat(backend): creating a fake db

---
+++ backend/Src/Config/db.js
@@ -0,0 +1,6 @@
+const users = [
+    { id: 1, name: "Karin Stela", email: "karin@gmail.com", password: "123", desc: "Karin Stela é a Presidente da empresa, liderando com visão estratégica e foco no crescimento sustentável. Com forte capacidade de gestão e tomada de decisão, ela orienta as diretrizes corporativas, fortalece a cultura organizacional e impulsiona a inovação. Seu compromisso com excelência e transparência garante o alinhamento entre as equipes e o avanço contínuo da empresa." },
+    { id: 2, name: "Wander Delgado", email: "wander@gmail.com", password: "123", desc: "Wander Delgado é o Diretor Financeiro, responsável pela gestão estratégica dos recursos da empresa. Com visão analítica e foco em resultados, ele conduz o planejamento financeiro, otimiza processos e garante uma administração eficiente e transparente." }
+]
+
+module.exports = users
--- backend/Src/Server.js
+++ backend/Src/Server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 2000
 const cors = require('cors')
+const db = require('./Config/db')
 
 app.use(cors())
 
