ff192a82d573aa1be1009ced45d9943eb044a8ae
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-01-30 18:54:03 -0300

    feat(backend): creating deleteUser route

--- backend/Src/Routes/deleteCategory.js
+++ backend/Src/Routes/deleteCategory.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../Config/db')
 
-router.post('/', async (req, res) => {
+router.delete('/', async (req, res) => {
     
     const categoria = req.body
 
