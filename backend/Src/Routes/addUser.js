8eb2785e8963f4a8b98a955ea25ea488595bfbb2
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-02-04 18:08:18 -0300

    feat(backend): implementing bycript para criptografar as senhas

--- backend/Src/Routes/addUser.js
+++ backend/Src/Routes/addUser.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../Config/db')
+const bycript = require('bcrypt')
+const saltRounds = 10
 
 router.post('/', async (req, res) => {
-    
+
     const user = req.body
 
     console.log("Acessando a rota de adicionar usuário")
@@ -11,24 +13,29 @@ router.post('/', async (req, res) => {
 
     const conn = await db()
 
-    try {
-        const [query] = await conn.query(
-            `INSERT INTO USERS (name, email, password, description, created_at) VALUES (?, ?, ?, ?, ?)`,
-            [user.name, user.email, user.password, user.desc, user.created_at]
-        )
+    bycript.hash(user.password, saltRounds, async (err, hash) => {
+        if (err) throw err
+        console.log("Senha criptografada", hash)
 
-        if (query.affectedRows === 1) {
-            console.log("Usuário inserido com sucesso no banco de dados")
-            res.status(200).send("Usuário inserido com sucesso no banco de dados")
-            return
-        } else {
-            console.log("Falha ao inserir usuário ao banco de dados")
-            res.status(404).send("Falha ao inserir usuário ao banco de dados")
-            return
+        try {
+            const [query] = await conn.query(
+                `INSERT INTO USERS (name, email, password, description, created_at) VALUES (?, ?, ?, ?, ?)`,
+                [user.name, user.email, hash, user.desc, user.created_at]
+            )
+
+            if (query.affectedRows === 1) {
+                console.log("Usuário inserido com sucesso no banco de dados")
+                res.status(200).send("Usuário inserido com sucesso no banco de dados")
+                return
+            } else {
+                console.log("Falha ao inserir usuário ao banco de dados")
+                res.status(404).send("Falha ao inserir usuário ao banco de dados")
+                return
+            }
+        } catch (error) {
+            console.error(error)
         }
-    } catch (error) {
-        console.error(error)
-    }
+    })
 
 })
 
