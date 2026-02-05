466e16a924a9528d2a444ee5ab113ba8d1bc8577
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-02-05 20:04:20 -0300

    feat(frontend): implementing resetPassword on frontend

--- frontend/src/Pages/RecuperarSenha/RecuperarSenha.jsx
+++ frontend/src/Pages/RecuperarSenha/RecuperarSenha.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as L from '../Login/Styles'
 import logo from '../../assets/logo.webp'
 import { MdOutlineLock } from "react-icons/md";
 import { LuEyeClosed } from "react-icons/lu";
 import { FaClockRotateLeft } from "react-icons/fa6";
+import { toast } from 'react-toastify';
+import { TbEye } from "react-icons/tb";
 
 export const RecuperarSenha = () => {
+
+    const [novaSenha, setNovaSenha] = useState({
+        senha: "",
+        confirmacaoSenha: ""
+    })
+
+    const checkPasswords = () => {
+        if (novaSenha.senha !== novaSenha.confirmacaoSenha) {
+            toast.error("Senhas não são iguais, por favor tente novamente.")
+            return
+        }
+
+
+    }
+
+    // função para mostrar a senha
+    const [type, setType] = useState("password")
+    const showPassword = (e) => {
+        if (type === "password") {
+            setType("text")
+        } else {
+            setType("password")
+        }
+    }
+
     return (
         <L.login>
             <L.loginContainer>
 
                 <L.loginHeader>
-                    <FaClockRotateLeft size={28}/>
+                    <FaClockRotateLeft size={28} />
                     <p>Redefinir senha</p>
                 </L.loginHeader>
 
@@ -28,8 +55,8 @@ export const RecuperarSenha = () => {
 
                             <span>
                                 <MdOutlineLock size={45} />
-                                <input type="password" name="novasenha" placeholder='Digite sua nova senha' />
-                                <button><LuEyeClosed size={45} /></button>
+                                <input type={type} name="novasenha" placeholder='Digite sua nova senha' value={novaSenha.senha} onChange={(e) => setNovaSenha({ ...novaSenha, senha: e.target.value })} />
+                                <button onClick={(e) => showPassword(e)}>{type === "password" ? <LuEyeClosed size={45} /> : <TbEye size={45} />}</button>
                             </span>
 
                         </L.inputContainer>
@@ -40,12 +67,12 @@ export const RecuperarSenha = () => {
 
                             <span>
                                 <MdOutlineLock size={45} />
-                                <input type="password" name="senha" placeholder='Confirma nova senha' />
-                                <button><LuEyeClosed size={45} /></button>
+                                <input type={type} name="senha" placeholder='Confirma nova senha' value={novaSenha.confirmacaoSenha} onChange={(e) => setNovaSenha({ ...novaSenha, confirmacaoSenha: e.target.value })} />
+                                <button onClick={(e) => showPassword(e)}>{type === "password" ? <LuEyeClosed size={45} /> : <TbEye size={45} />}</button>
                             </span>
 
                             <div className="buttonContainer">
-                                <button id='entrar'>REDEFINIR SENHA</button>
+                                <button id='entrar' onClick={() => checkPasswords()}>REDEFINIR SENHA</button>
                             </div>
 
                             <a href="/login">Entrar?</a>
