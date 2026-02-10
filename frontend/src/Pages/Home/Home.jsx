dc612ba6b67449d3e83274622d1baf6494d4e86b
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-02-10 13:41:56 -0300

    feat(home.jsx): implementing useEffect to fetch Users, Posts & categories

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import * as H from './Styles'
 import { Hero } from '../../Components/Hero/Hero'
 import { Sobre } from '../../Components/Sobre/Sobre'
@@ -8,8 +8,18 @@ import { Missao } from '../../Components/Missao/Missao'
 import { Feedbacks } from '../../Components/Feedbacks/Feedbacks'
 import { Blog } from '../../Components/Blog/Blog'
 import { Localizacao } from '../../Components/Localizacao/Localizacao'
+import { SalusContext } from '../../Context/Context'
 
 export const Home = () => {
+
+  const {fetchUsers, fetchCategories, fetchPosts} = useContext(SalusContext)
+
+  useEffect(() => {
+    fetchCategories()
+    fetchPosts()
+    fetchUsers()
+  }, [])
+
   return (
     <H.home>
         <Hero/>
