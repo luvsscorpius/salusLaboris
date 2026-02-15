50405299eae008de85e7bc47c356e5fc5a895cb3
Anderson Vitor <andersonvitor@MacBook-Air-de-Anderson.local>
2026-02-15 14:11:59 -0300

    feat(feedbacks): adding feedbacks

--- frontend/src/Components/Feedbacks/Feedbacks.jsx
+++ frontend/src/Components/Feedbacks/Feedbacks.jsx
@@ -6,11 +6,7 @@ import petrobrasLogo from '../../assets/logos/petrobrasLogo.webp'
 
 export const Feedbacks = () => {
     const feedbacks = [
-        {id: 0, empresa: "Logo empresa Exxer", logo: exxerLogo, feedback: "Com a parceria da Salus Laboris conseguimos reduzir significativamente os índices de afastamento por questões ocupacionais. O suporte da equipe foi fundamental para a implementação de programas de saúde preventiva.", nome: "Carlos Mendes", cargo: "Diretor de Operações"},
-
-        {id: 1, empresa: "Logo empresa EnterPlak", logo: enterplakLogo, feedback: "A atuação da Salus Laboris trouxe mais confiança para nossos colaboradores. Hoje temos um ambiente de trabalho mais saudável, produtivo e alinhado às normas de segurança.", nome: "Fernanda Oliveira", cargo: "Gerente de RH"},
-        
-        {id: 2, empresa: "Logo empresa Petrobras", logo: petrobrasLogo, feedback: "A Salus Laboris nos apoia na gestão de saúde ocupacional em grande escala, com soluções técnicas e acompanhamento próximo. Uma parceria estratégica para manter nossa força de trabalho segura e engajada.", nome: "João Ribeiro", cargo: "Coordenador de Saúde"},
+        {id: 0, empresa: "Logo empresa Exxer", logo: exxerLogo, feedback: "Wander e toda a sua equipe desempenham com maestria o trabalho a que se propõem. Demonstram atenção e comprometimento com as normas vigentes. Recomendo fortemente o trabalho realizado e ressalto sua competência e profissionalismo.", nome: "Ednéa Ribeiro", cargo: "Analista de Recursos Humanos I Gestão de Pessoas"},
     ]
 
   return (
